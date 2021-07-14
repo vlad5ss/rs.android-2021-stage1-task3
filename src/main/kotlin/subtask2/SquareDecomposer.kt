ebe923bbd89c8b742cd68113bf01c3691fdb29fc
vlad5ss <vlad5grup@gmail.com>
2021-07-14 09:00:59 +0300

    added

--- src/main/kotlin/subtask2/SquareDecomposer.kt
+++ src/main/kotlin/subtask2/SquareDecomposer.kt
@@ -4,6 +4,25 @@ class SquareDecomposer {
 
     // TODO: Complete the following function
     fun decomposeNumber(number: Int): Array<Int>? {
-        throw NotImplementedError("Not implemented")
+        return if (number < 3) null
+        else finder(square(number), number - 1)?.toTypedArray()
     }
+
+    private fun finder(number: Int, position: Int): ArrayList<Int>? {
+        for (i in position downTo 1) {
+            val rest = number - square(i)
+            if (rest == 0) return arrayListOf(i)
+            if (rest < 0) return null
+            var radical = kotlin.math.sqrt(rest.toDouble()).toInt()
+            if (radical >= i) radical = i - 1
+            val result = finder(rest, radical)
+            if (result != null) {
+                result.add(i)
+                return result
+            }
+        }
+        return null
+    }
+
+    private fun square(x: Int) = x * x
 }
